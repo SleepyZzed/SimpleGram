c478391f1d9c2ccaf3f1bfd9451cf23a6e767cb3
Zed Amaar <mcverdict@gmail.com>
2023-05-04 20:51:12 +0100

    Updates

---
+++ precache-manifest.1f0f8070687cdf2805fb2dee53911e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c336cdcfc76854ee8b996633930fb1b",
+    "url": "/%20SimpleGram/index.html"
+  },
+  {
+    "revision": "840822756bfb3ac74765",
+    "url": "/%20SimpleGram/static/css/main.7a76ddbb.chunk.css"
+  },
+  {
+    "revision": "9374eb84c28ad89d927a",
+    "url": "/%20SimpleGram/static/js/2.a32f5d45.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/%20SimpleGram/static/js/2.a32f5d45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "840822756bfb3ac74765",
+    "url": "/%20SimpleGram/static/js/main.3c63b423.chunk.js"
+  },
+  {
+    "revision": "8385dd72c870bfce6e99",
+    "url": "/%20SimpleGram/static/js/runtime-main.b4eec6cb.js"
+  }
+]);
